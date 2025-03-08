a77922795edc38d4e1f5b2bf8e4343ed30b4e925
hiraekram10 <heraiqram123@gmail.com>
2025-03-08 23:53:28 +0500

    navbar responsive, cart fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -21,28 +21,30 @@ const Navigation = () => {
 
 	return (
 		<nav className='bg-white shadow-lg'>
-			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-				<div className='flex items-center justify-between h-16'>
-					<div className='flex-shrink-0'>
+			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 '>
+				<div className='flex items-center justify-between h-16  w-full'>
+					<div className='flex-shrink-0 '>
 						<Link to='/'>
-							<img className='h-10 md:h-6' src={logo} alt='logo' />
+						<div className=''>
+							<img className=' h-7 md:h-7 lg:h-7 xl:h-7' src={logo} alt='logo' />
+							</div>
 							<span className='font-bold items-center border-b-[3px] hover:border-[#e57312] transition duration-500'>
 								rofessoR
 							</span>
-							<span className='font-bold border-b-[3px] hover:border-[#4bf6d4] transition duration-800'>Store</span>
+							<span className=' font-bold border-b-[3px] hover:border-[#4bf6d4] transition duration-800'>Store</span>
 						</Link>
 					</div>
 
 					{/* Desktop Menu */}
 					<div className='hidden sm:block sm:ml-6'>
-						<div className='flex space-x-4'>
-							<NavLink to='/' icon={<FaHome />} text='Home' />
-							<NavLink to='/products' text='Products' />
-							<NavLink to='/about' text='About' />
-							<NavLink to='/contact' text='Contact' />
+						<div className='flex space-x-2 md:space-x-8'>
+							<NavLink to='/' icon={<FaHome />} text='Home' className='text-red-300' />
+							<NavLink to='/products' text='Products' className='text-red-300' />
+							<NavLink to='/about' text='About' className='text-red-300' />
+							<NavLink to='/contact' text='Contact' className='text-red-300' />
 							{!isAuthenticated && (
 								<>
-									<NavLink to='/Signup' text='Sign Up' />
+									<NavLink to='/Signup'  text='Sign Up' />
 									<NavLink to='/login' text='Login' />
 								</>
 							)}
@@ -50,13 +52,15 @@ const Navigation = () => {
 					</div>
 
 					{/* Cart Icon */}
-					<div className={`absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0`}>
-						<Link to='/cart' className='items-center hidden lg:flex'>
-							<div className='relative'>
-								<FaShoppingCart className='h-6 w-6 sm:h-8 sm:w-8' />
+					<div className={`absolute right-14 flex items-center pr-2 sm:static  sm:ml-6 sm:pr-0`}>
+						<Link to='/cart' className='items-center   '>
+							<div className='relative right-0 sm:right-4 md:top-0 lg:top-0  xl:top-0 '>
+								<FaShoppingCart className='h-6 w-6 sm:h-8 sm:w-8 md:w-6 text-gray-700' />
+								
 								{totalCartQuantity > 0 && (
 									<span className='absolute top-0 right-0 inline-flex items-center justify-center px-1 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full'>
-										{totalCartQuantity}
+										{totalCartQuantity} 
+										
 									</span>
 								)}
 							</div>
@@ -76,12 +80,12 @@ const Navigation = () => {
 
 			{/* Mobile Menu */}
 			<div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`}>
-				<div className='px-2 pt-2 pb-3 space-y-1'>
-					<NavLinkMobile to='/' text='Home' onClick={toggleMenu} />
-					<NavLinkMobile to='/products' text='Products' onClick={toggleMenu} />
-					<NavLinkMobile to='/about' text='About' onClick={toggleMenu} />
-					<NavLinkMobile to='/contact' text='Contact' onClick={toggleMenu} />
-					<NavLinkMobile to='/cart' text='Cart' onClick={toggleMenu} />
+				<div className='px-2 pt-2 pb-3 mx-auto space-y-1'>
+					<NavLinkMobile to='/' text='Home' onClick={toggleMenu}  />
+					<NavLinkMobile to='/products' text='Products' onClick={toggleMenu}  />
+					<NavLinkMobile to='/about' text='About' onClick={toggleMenu}  />
+					<NavLinkMobile to='/contact' text='Contact' onClick={toggleMenu}  />
+					{/* <NavLinkMobile to='/cart' text='Cart' onClick={toggleMenu}  /> */}
 				</div>
 			</div>
 		</nav>
@@ -92,7 +96,7 @@ const Navigation = () => {
 const NavLink = ({ to, icon, text }) => (
 	<Link
 		to={to}
-		className='flex items-center text-gray-700 hover:text-orange-500 transition duration-300 relative group'>
+		className='flex items-center font-semibold text-gray-700 hover:text-orange-500 transition duration-300 relative group'>
 		<span className='mr-2'>{icon}</span>
 		<span>{text}</span>
 		<span className='absolute bottom-0 left-0 w-full bg-[#4bf6d4] h-0.5 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-500 origin-left'></span>
